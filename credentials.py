b6b1b80c9e06819ef256956b546d9cd9b7d33e17
Kevin Lindemark Holm <KELH@kea.dk>
2023-09-10 17:50:12 +0200

    prog #2 MQTT example

---
+++ credentials.py
@@ -0,0 +1,16 @@
+# Python dictionary {} der indeholder login oplysninger
+# Undlad at uploade denne fil til github når den er udfyldt
+
+# ADAFRUIT_IO_URL behøves ikke at ændres
+
+# ADAFRUIT_IO_FEEDNAME behøves ikke at ændres
+# men der skal oprettes en feed med samme navn på adafruit.io
+
+credentials = {
+    'ssid' : 'DIT WIFI HOTSPOT/ACCESS POINT NAVN',
+    'password' : 'DIT WIFI HOTSPOT/ACCESS POINT PASSWORD',
+    'ADAFRUIT_IO_URL' : b'io.adafruit.com',
+    'ADAFRUIT_USERNAME' : b'DIT ADAFRUIT BRUGERNAVN',
+    'ADAFRUIT_IO_KEY' : b'DIN ADAFRUIT IO KEY',
+    'ADAFRUIT_IO_FEEDNAME' : b'ESP32feed'
+    }
